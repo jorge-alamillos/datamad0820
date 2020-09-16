637f203ac21bc80f44beffc5943aaf2d1d5e3406
jorge-alamillos <jorgealamillos@gmail.com>
2020-09-16 22:29:00 +0200

    resuelto

--- module-1/lab-code-simplicity-efficiency/your-code/challenge-2.py
+++ module-1/lab-code-simplicity-efficiency/your-code/challenge-2.py
@@ -1,38 +1,28 @@
-"""
-The code below generates a given number of random strings that consists of numbers and 
-lower case English letters. You can also define the range of the variable lengths of
-the strings being generated.
+import string 
+import random
 
-The code is functional but has a lot of room for improvement. Use what you have learned
-about simple and efficient code, refactor the code.
-"""
+a = input('Enter minimum string length: ')
+b = input('Enter maximum string length: ')
+n = input('How many random strings to generate? ')
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
+def RandomStringGenerator(l=12, a=(list(string.ascii_lowercase) + list(str(i) for i in range(0,10)))):
     p = 0
     s = ''
     while p<l:
-        import random
         s += random.choice(a)
         p += 1
     return s
 
 def BatchStringGenerator(n, a=8, b=12):
     r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
+    for i in range(int(n)):
+        if int(a) < int(b):
             c = random.choice(range(a, b))
         elif a == b:
             c = a
         else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
+            raise Exception("Incorrect min and max string lengths. Try again.")
         r.append(RandomStringGenerator(c))
     return r
-
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
-
+​
 print(BatchStringGenerator(int(n), int(a), int(b)))
